12d7f168c0e77ffc0fc8d1c344e183d7c2e5ebe5
romanitwork <workromanmail@gmail.com>
2020-02-21 15:35:22 +0200

    fixed task13

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './styles.css';
 import pixApi from './API/apiService.js';
-import imgTemplate from './templates/form.hbs';
+import imgTemplate from './templates/imageTemplate.hbs';
 const container = document.querySelector('.container');
 const button = document.querySelector('.load-more');
 const input = document.querySelector('input');
 const search = document.querySelector('#search-form');
-const STAR = 'star';
+// import starGold from './images/star-gold.png';
+// import starEmpty from './images/star-empty.png';
+// const STAR = 'star';
 let currentPage = 0;
 button.addEventListener('click', loadOnClick);
-search.addEventListener('keypress', loadInputValue);
+search.addEventListener('submit', loadInputValue);
 function parseData(data) {
   const containerPoint = imgTemplate(data);
   container.insertAdjacentHTML('beforeend', containerPoint);
@@ -27,22 +29,22 @@ let _localStorage = {};
 if (localStorage.getItem(STAR)) {
   _localStorage = JSON.parse(localStorage.getItem(STAR));
 }
-container.addEventListener('click', onClick);
-function onClick(e) {
- 
-  console.log(e);
-  if (e.target.className === 'favorite-item') {
-    const currentImgSrc = e.target.parentNode.querySelector('.photo-item').src;
-    console.log(_localStorage[currentImgSrc]);
-    _localStorage[currentImgSrc] =
-      _localStorage[currentImgSrc] === true ? false : true;
-      console.log(_localStorage[currentImgSrc]);
-    if (_localStorage[currentImgSrc] === true) {
-      e.target.setAttribute('src', './images/star-gold.png');
-    } else {
-      e.target.setAttribute('src', './images/star-empty.png');
-    }
-    localStorage.setItem(STAR, JSON.stringify(_localStorage));
-    console.log(_localStorage[currentImgSrc]);
-  }
-}
+// container.addEventListener('click', onClick);
+// function onClick(e) {
+
+//   console.log(e);
+//   if (e.target.className === 'favorite-item') {
+//     const currentImgSrc = e.target.parentNode.querySelector('.photo-item').src;
+//     console.log(_localStorage[currentImgSrc]);
+//     _localStorage[currentImgSrc] =
+//       _localStorage[currentImgSrc] === true ? false : true;
+//       console.log(_localStorage[currentImgSrc]);
+//     if (_localStorage[currentImgSrc] === true) {
+//       e.target.setAttribute('src', starGold);
+//     } else {
+//       e.target.setAttribute('src', starEmpty);
+//     }
+//     localStorage.setItem(STAR, JSON.stringify(_localStorage));
+//     console.log(_localStorage[currentImgSrc]);
+//   }
+// }
